2e6bf601bd9dadb37088178067518d1d8539b1d1
Konrad Kubczyk <konrad@kubczyk.com>
2024-07-30 23:34:38 +0200

    fix: minor layout tweaks

--- src/components/ResultField.tsx
+++ src/components/ResultField.tsx
@@ -1,17 +1,18 @@
 export default function ResultField({hash}: { hash: string }) {
-    function highlightHash(e: React.MouseEvent<HTMLInputElement>): void {
-        const inputField: HTMLInputElement = e.target as HTMLInputElement;
+    function highlightHash(e: React.MouseEvent<HTMLTextAreaElement>): void {
+        const inputField = e.target as HTMLTextAreaElement;
         inputField.select();
     }
 
     return (
         <>
             <label htmlFor={hash} hidden aria-hidden="false">Result</label>
-            <input
+            <textarea
                 id="result"
                 onClick={highlightHash}
                 value={hash}
                 readOnly={true}
+                rows={4}
             />
         </>
     );
