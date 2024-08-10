9a8003d1bdc04a1430838ea37063fdad79f61a29
Konrad Kubczyk <konrad@kubczyk.com>
2024-08-10 14:37:41 +0200

    chore: code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import bgImage from './assets/bg.png'
 
 function App() {
     return (
-        <div className="content-wrapper" style={{ backgroundImage: `url(${bgImage})`}}>
-        <NoisyContainer>
-            <main id="home">
-                <div id="intro">
-                    <h1>h<span className="logo-v">v</span>sh</h1>
-                    <p>
-                        Select an algorithm and type or paste your text to see its hash below.
-                    </p>
-                </div>
-                <HashForm/>
-            </main>
-        </NoisyContainer>
+        <div className="content-wrapper" style={{backgroundImage: `url(${bgImage})`}}>
+            <NoisyContainer>
+                <main id="home">
+                    <div id="intro">
+                        <h1>h<span className="logo-v">v</span>sh</h1>
+                        <p>
+                            Select an algorithm and type or paste your text to see its hash below.
+                        </p>
+                    </div>
+                    <HashForm/>
+                </main>
+            </NoisyContainer>
         </div>
     )
 }
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,8 +1,6 @@
 export async function calculateHash(algorithm: string, text: string): Promise<string> {
-
     const encoder = new TextEncoder();
     const data = encoder.encode(text);
-
     const hash = await crypto.subtle.digest(algorithm, data);
     return arrayBufferToHex(hash);
 }
@@ -10,10 +8,12 @@ export async function calculateHash(algorithm: string, text: string): Promise<st
 function arrayBufferToHex(buffer: ArrayBuffer): string {
     const view = new DataView(buffer);
     const result = [];
+
     for (let i = 0; i < view.byteLength; i += 4) {
         const value = view.getUint32(i) >>> 0;
         const hex = value.toString(16);
         result.push(hex.padStart(8, '0'));
     }
+
     return result.join('');
 }
