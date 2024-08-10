09486f3ed68e7d5646a790d97f5f62f9d098dba9
Konrad Kubczyk <konrad@kubczyk.com>
2024-08-10 13:35:51 +0200

    feat: visual improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css'
 import HashForm from './components/HashForm.tsx';
+import NoisyContainer from "./components/NoisyContainer.tsx";
 
 function App() {
     return (
-        <main id="home">
-            <div id="intro">
-                <h1>h<span className="logo-v">v</span>sh</h1>
-                <p>
-                    Select an algorithm and type or paste your text to see its hash below.
-                </p>
-            </div>
-            <HashForm/>
-        </main>
+        <NoisyContainer>
+            <main id="home">
+                <div id="intro">
+                    <h1>h<span className="logo-v">v</span>sh</h1>
+                    <p>
+                        Select an algorithm and type or paste your text to see its hash below.
+                    </p>
+                </div>
+                <HashForm/>
+            </main>
+        </NoisyContainer>
     )
 }
 
